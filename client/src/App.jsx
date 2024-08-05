08688125297bc12d3567c919f974d4f5c35a40e1
Ndigithe-john <ndigithejohn@gmail.com>
2024-08-05 16:17:47 +0300

    added the error screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Layout from '@components/Layout';
 import HomeScreen from '@screens/Home';
+import ErrorScreen from '@screens/Error';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorScreen />,
     children: [
       {
         index: true,
