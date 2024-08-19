a77d68c5ffd257e2b37031cf509fd4fc61c62aa0
Ndigithe-john <ndigithejohn@gmail.com>
2024-08-19 11:51:15 +0300

    Separated the product routes into its own file and fetched data from the database

--- server/main.js
+++ server/main.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import colors from "colors";
 
-import products from "#data/product.data.js";
 import connectDB from "#config/db.config.js";
+import ProductRoutes from "#routes/product.route.js";
 
 dotenv.config();
 
@@ -18,14 +18,8 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+app.use("/api/v1/products", ProductRoutes);
 
-app.get("/api/v1/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/v1/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 app.listen(port, () => {
   console.log(
     `Server is running on ${process.env.NODE_ENV} on port ${port}`.bgYellow.bold
