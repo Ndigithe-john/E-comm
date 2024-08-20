9ba66d4d1d9d4d53a45a8b8a8c825e929fb23308
Ndigithe-john <ndigithejohn@gmail.com>
2024-08-20 21:42:30 +0300

    added the products api redux slice, Modified the homescreen to use the productApiSlice

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -1,29 +1,26 @@
-import Axios from 'axios';
-import { useState, useEffect } from 'react';
-
 import ProductCard from '@components/ProductCard';
 
-const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+import { useGetProductsQuery } from '@slices/productApiSlice';
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await Axios.get('/api/v1/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+const HomeScreen = () => {
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
   return (
     <section className='bg-white'>
       <div className='mx-w-7xl mx-auto px-3 py-10 sm:px-6 lg:px-8'>
         <h1 className='text-2xl font-bold text-slate-900'>Latest Products</h1>
 
-        <div className='mg:grid-cols-3 mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-          {products.map((product) => (
-            <ProductCard product={product} key={product._id} />
-          ))}
-        </div>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : isError ? (
+          <p>{error.data.message || error.message}</p>
+        ) : (
+          <div className='mg:grid-cols-3 mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+            {products.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
---
+++ client/src/slices/productApiSlice.js
@@ -0,0 +1,16 @@
+import { PRODUCT_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const productApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCT_URL,
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ['Product'],
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productApiSlice;
