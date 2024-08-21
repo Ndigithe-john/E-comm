2b071191c223fe39d20e1820204d5d083fba2fba
Ndigithe-john <ndigithejohn@gmail.com>
2024-08-21 13:00:14 +0300

    Added add to cart redux action/reducer function

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -4,10 +4,49 @@ const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
 
+const addDecimals = (num) => num.toFixed(2);
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.cartItems.find(
+        (cartItem) => cartItem._id === item._id,
+      );
+      if (existingItem) {
+        state.cartItems = state.cartItems.map((cartItem) => {
+          return cartItem._id === existingItem._id ? item : cartItem;
+        });
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //Calculate the total price of the items
+
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce(
+          (acc, currVal) => acc + currVal.price * currVal.qty,
+          0,
+        ),
+      );
+
+      //calculate shipping price
+      state.shippingPrice = addDecimals(+state.itemsPrice > 5000 ? 0 : 3000);
+
+      // Calculatte tax price
+      state.taxPrice = addDecimals(0.18 * +state.itemsPrice);
+
+      //calculate the total price
+      state.totalPrice = addDecimals(
+        +state.itemsPrice + +state.shippingPrice + +state.taxPrice,
+      );
+
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
