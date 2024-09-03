081ef562c86ad80e964a5ec4f5f871803765e08e
Ndigithe-john <ndigithejohn@gmail.com>
2024-09-03 21:05:42 +0300

    Added logic to generate JWT

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 import UserModel from "#models/user.model.js";
 
 /**
@@ -11,7 +13,19 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await UserModel.findOne({ email });
+
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
