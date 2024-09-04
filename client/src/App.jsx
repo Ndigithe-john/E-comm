fc30355a259d94c9aa772876692ea036f541db38
Ndigithe-john <ndigithejohn@gmail.com>
2024-09-04 14:45:08 +0300

    added shipping screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {
   CartScreen,
   LoginScreen,
   RegisterScreen,
+  ShippingScreen,
 } from '@screens';
 
 const router = createBrowserRouter([
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
       {
         path: '/register',
         element: <RegisterScreen />,
+      },
+      {
+        path: '/shipping',
+        element: <ShippingScreen />,
       },
     ],
   },
--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -4,3 +4,4 @@ export { default as ErrorScreen } from './Error';
 export { default as ProductDetailsScreen } from './ProductDetails';
 export { default as LoginScreen } from './Login';
 export { default as RegisterScreen } from './Register';
+export { default as ShippingScreen } from './Shipping';
